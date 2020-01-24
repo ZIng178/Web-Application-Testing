a69ca6771cb662467ad445e51c7729dc6fcc5f5e
Wangdi Tenzing <wangdi457@gmail.com>
2020-01-24 01:39:22 -0500

    Initial commit Added testing dependencies Created a two state hooks for ball and strike

---
+++ tenzing/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+ const [strike, setStrike]=useState(0);
+ const [ball, setBall]=useState(0);
+
+ const addStrikes= currentStrike =>{
+   if(currentStrike >=2){
+     setStrikeCount(0)
+     setBallCount(0)
+    }else{
+      setStrikeCount(strikeCount+1)
+    }
+ }
+
+ const addBalls = currentElements
+
+  return (
+    
+  );
+}
+
+export default App;
